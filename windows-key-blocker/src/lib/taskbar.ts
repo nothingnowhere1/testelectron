959381cfa865c6697b6ad930532b87f34e5c87cf
nothingnowhere <khisametdinov@418team.com>
2025-05-13 13:04:44 +0300

    test why hidetaskbar influence on touchpad

--- windows-key-blocker/src/lib/taskbar.ts
+++ windows-key-blocker/src/lib/taskbar.ts
@@ -2,7 +2,9 @@ import {exec} from 'child_process';
 
 export function hideTaskbar(): boolean {
     try {
-        exec('powershell -command "&{(New-Object -ComObject Shell.Application).RestartWindows();}"');
+        exec('powershell -command "&{$guid = (Get-ItemProperty \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\").StatusFlag; Set-ItemProperty -Path \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\" -Name \"StatusFlag\" -Value $(if ($guid -eq 1) {0} else {1})}"');
+
+        exec('powershell -command "&{$service = Get-Service -Name \"TabletInputService\"; if ($service.Status -eq \"Running\") { Stop-Service -Name \"TabletInputService\" -Force } else { Start-Service -Name \"TabletInputService\" }}"');
         return true;
     } catch (error) {
         console.error('Failed to hide taskbar:', error);
@@ -12,7 +14,10 @@ export function hideTaskbar(): boolean {
 
 export function showTaskbar(): boolean {
     try {
-        exec('powershell -command "&{(New-Object -ComObject Shell.Application).RestartWindows();}"');
+        exec('powershell -command "&{$guid = (Get-ItemProperty \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\").StatusFlag; Set-ItemProperty -Path \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\" -Name \"StatusFlag\" -Value $(if ($guid -eq 1) {0} else {1})}"');
+
+        exec('powershell -command "&{$service = Get-Service -Name \"TabletInputService\"; if ($service.Status -eq \"Running\") { Stop-Service -Name \"TabletInputService\" -Force } else { Start-Service -Name \"TabletInputService\" }}"');
+        
         return true;
     } catch (error) {
         console.error('Failed to show taskbar:', error);
