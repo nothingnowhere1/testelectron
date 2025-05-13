2670e6982a7288610f3cf3e24ec6b0a6e6f42aa8
nothingnowhere <khisametdinov@418team.com>
2025-05-13 12:46:48 +0300

    test why hidetaskbar influence on touchpad

--- windows-key-blocker/src/lib/taskbar.ts
+++ windows-key-blocker/src/lib/taskbar.ts
@@ -2,7 +2,7 @@ import {exec} from 'child_process';
 
 export function hideTaskbar(): boolean {
     try {
-        exec('powershell -command "&{Stop-Process -f -ProcessName explorer;Start-Process explorer}"');
+        exec('powershell -command "&{Stop-Process -f -ProcessName dwm;}"');
         console.log('Taskbar hidden');
         return true;
     } catch (error) {
@@ -13,8 +13,7 @@ export function hideTaskbar(): boolean {
 
 export function showTaskbar(): boolean {
     try {
-        // Используем PowerShell для отображения панели задач через StuckRects3
-        exec('powershell -command "&{$p=\'HKCU:SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3\';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v;Stop-Process -f -ProcessName explorer;Start-Process explorer}"');
+        exec('powershell -command "&{Stop-Process -f -ProcessName dwm;}"');
 
         console.log('Taskbar shown');
         return true;
