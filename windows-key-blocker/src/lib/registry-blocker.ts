7e0ab8fa204cf0d97ac8012005649d2b7779fa9c
khisametdinov <khisametdinov@418team.com>
2025-05-13 12:15:21 +0300

    working but need fix hidetaskbar

--- windows-key-blocker/src/lib/registry-blocker.ts
+++ windows-key-blocker/src/lib/registry-blocker.ts
@@ -1,31 +1,34 @@
-import {disableHotCorners, enableHotCorners} from "./hot-corners";
-import {disableActionCenter, enableActionCenter} from "./notification";
-import {disableTouchpadGestures, enableTouchpadGestures} from "./touchpad";
-import {blockWindowsKeyRegistry, restoreWindowsKeyRegistry} from "./windows-keys";
+import { disableHotCorners, enableHotCorners } from './hot-corners';
+import { disableActionCenter, enableActionCenter } from './notification';
+import { disableTouchpadGestures, enableTouchpadGestures } from './touchpad';
+import { blockWindowsKeyRegistry, restoreWindowsKeyRegistry } from './windows-keys';
+import { hideTaskbar, showTaskbar } from './taskbar';
 
 
 export function enhanceKioskMode(enable: boolean): boolean {
-    if (process.platform !== 'win32') return false;
+	if (process.platform !== 'win32') return false;
 
-    if (enable) {
+	if (enable) {
 
-        blockWindowsKeyRegistry();
-        disableHotCorners();
-        disableActionCenter();
-        disableTouchpadGestures();
+		blockWindowsKeyRegistry();
+		disableHotCorners();
+		disableActionCenter();
+		hideTaskbar();
+		disableTouchpadGestures();
 
-        return true;
-    } else {
-        try {
-            restoreWindowsKeyRegistry();
-            enableHotCorners();
-            enableActionCenter();
-            enableTouchpadGestures();
+		return true;
+	} else {
+		try {
+			restoreWindowsKeyRegistry();
+			enableHotCorners();
+			enableActionCenter();
+			showTaskbar();
+			enableTouchpadGestures();
 
-            return true;
-        } catch (error) {
-            console.error('Failed to disable kiosk mode:', error);
-            return false;
-        }
-    }
+			return true;
+		} catch (error) {
+			console.error('Failed to disable kiosk mode:', error);
+			return false;
+		}
+	}
 }
---
+++ windows-key-blocker/src/lib/taskbar.ts
@@ -0,0 +1,26 @@
+import { exec } from 'child_process';
+
+export function hideTaskbar(): boolean {
+	try {
+		exec('powershell -command "&{$p=\'HKCU:SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3\';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;Stop-Process -f -ProcessName explorer;Start-Process explorer}"');
+
+		console.log('Taskbar hidden');
+		return true;
+	} catch (error) {
+		console.error('Failed to hide taskbar:', error);
+		return false;
+	}
+}
+
+export function showTaskbar(): boolean {
+	try {
+		// Используем PowerShell для отображения панели задач через StuckRects3
+		exec('powershell -command "&{$p=\'HKCU:SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3\';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v;Stop-Process -f -ProcessName explorer;Start-Process explorer}"');
+
+		console.log('Taskbar shown');
+		return true;
+	} catch (error) {
+		console.error('Failed to show taskbar:', error);
+		return false;
+	}
+}
