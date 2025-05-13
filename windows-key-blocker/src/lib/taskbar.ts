30cbea712da795a9a52815653b33215e70c5ab87
nothingnowhere <khisametdinov@418team.com>
2025-05-13 13:13:02 +0300

    test why hidetaskbar influence on touchpad

--- windows-key-blocker/src/lib/taskbar.ts
+++ windows-key-blocker/src/lib/taskbar.ts
@@ -2,7 +2,20 @@ import {exec} from 'child_process';
 
 export function hideTaskbar(): boolean {
     try {
-        exec('powershell -WindowStyle Hidden -Command "& {Stop-Process -Name explorer -Force; Start-Process explorer -ArgumentList \"/select,\\\"C:\\Windows\\explorer.exe\\\"\"}"');
+        exec('powershell -Command "& {' +
+            // Создаем временный VBS файл
+            '$vbsContent = \\"Set WshShell = CreateObject(\\\\\\"WScript.Shell\\\\\\")\\r\\n' +
+            'WshShell.Run \\\\\\"taskkill /f /im explorer.exe\\\\\\", 0, True\\r\\n' +
+            'WScript.Sleep 100\\r\\n' +
+            'WshShell.Run \\\\\\"explorer.exe /root,, /e,, /separate\\\\\\", 0, False\\r\\n\\"; ' +
+            // Записываем скрипт во временный файл
+            '$tempFile = [System.IO.Path]::GetTempFileName() + \\".vbs\\"; ' +
+            '[System.IO.File]::WriteAllText($tempFile, $vbsContent); ' +
+            // Запускаем скрипт без показа окон
+            'Start-Process -FilePath \\"wscript.exe\\" -ArgumentList $tempFile -WindowStyle Hidden -Wait; ' +
+            // Удаляем временный файл
+            'Remove-Item $tempFile -Force' +
+            '}"');
 
         return true;
     } catch (error) {
@@ -13,8 +26,21 @@ export function hideTaskbar(): boolean {
 
 export function showTaskbar(): boolean {
     try {
-        exec('powershell -WindowStyle Hidden -Command "& {Stop-Process -Name explorer -Force; Start-Process explorer -ArgumentList \"/select,\\\"C:\\Windows\\explorer.exe\\\"\"}"');
-
+        exec('powershell -Command "& {' +
+            // Создаем временный VBS файл
+            '$vbsContent = \\"Set WshShell = CreateObject(\\\\\\"WScript.Shell\\\\\\")\\r\\n' +
+            'WshShell.Run \\\\\\"taskkill /f /im explorer.exe\\\\\\", 0, True\\r\\n' +
+            'WScript.Sleep 100\\r\\n' +
+            'WshShell.Run \\\\\\"explorer.exe /root,, /e,, /separate\\\\\\", 0, False\\r\\n\\"; ' +
+            // Записываем скрипт во временный файл
+            '$tempFile = [System.IO.Path]::GetTempFileName() + \\".vbs\\"; ' +
+            '[System.IO.File]::WriteAllText($tempFile, $vbsContent); ' +
+            // Запускаем скрипт без показа окон
+            'Start-Process -FilePath \\"wscript.exe\\" -ArgumentList $tempFile -WindowStyle Hidden -Wait; ' +
+            // Удаляем временный файл
+            'Remove-Item $tempFile -Force' +
+            '}"');
+        
         return true;
     } catch (error) {
         console.error('Failed to show taskbar:', error);
