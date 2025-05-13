c695929c89939ac4ea3760eb09bafc0859c4205e
nothingnowhere <khisametdinov@418team.com>
2025-05-13 13:08:53 +0300

    test why hidetaskbar influence on touchpad

--- windows-key-blocker/src/lib/taskbar.ts
+++ windows-key-blocker/src/lib/taskbar.ts
@@ -2,9 +2,8 @@ import {exec} from 'child_process';
 
 export function hideTaskbar(): boolean {
     try {
-        exec('powershell -command "&{$guid = (Get-ItemProperty \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\").StatusFlag; Set-ItemProperty -Path \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\" -Name \"StatusFlag\" -Value $(if ($guid -eq 1) {0} else {1})}"');
+        exec('powershell -WindowStyle Hidden -Command "& {Stop-Process -Name explorer -Force; Start-Process explorer -ArgumentList \"/select,\\\"C:\\Windows\\explorer.exe\\\"\"}"');
 
-        exec('powershell -command "&{$service = Get-Service -Name \"TabletInputService\"; if ($service.Status -eq \"Running\") { Stop-Service -Name \"TabletInputService\" -Force } else { Start-Service -Name \"TabletInputService\" }}"');
         return true;
     } catch (error) {
         console.error('Failed to hide taskbar:', error);
@@ -14,10 +13,8 @@ export function hideTaskbar(): boolean {
 
 export function showTaskbar(): boolean {
     try {
-        exec('powershell -command "&{$guid = (Get-ItemProperty \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\").StatusFlag; Set-ItemProperty -Path \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\" -Name \"StatusFlag\" -Value $(if ($guid -eq 1) {0} else {1})}"');
+        exec('powershell -WindowStyle Hidden -Command "& {Stop-Process -Name explorer -Force; Start-Process explorer -ArgumentList \"/select,\\\"C:\\Windows\\explorer.exe\\\"\"}"');
 
-        exec('powershell -command "&{$service = Get-Service -Name \"TabletInputService\"; if ($service.Status -eq \"Running\") { Stop-Service -Name \"TabletInputService\" -Force } else { Start-Service -Name \"TabletInputService\" }}"');
-        
         return true;
     } catch (error) {
         console.error('Failed to show taskbar:', error);
